0619307918ae8eb62caaadf61118124f20fb36ce
Poojyabadramraju <140185431+Poojyabadramraju@users.noreply.github.com>
2023-11-21 11:10:40 +0530

    Update Welcome.java

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -3,7 +3,7 @@ package com.edubridge.app1;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Welcome to eclipse");
 	}
 
 }
